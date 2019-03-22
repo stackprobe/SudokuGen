fbfc798758acec38867d9d39b9d218a05540c3ec
stackprobe <stackprobes@gmail.com>
2019-03-23 00:31:41 +0900

    coffee break

--- Sudoku2/Sudoku2/System.h
+++ Sudoku2/Sudoku2/System.h
@@ -20,8 +20,13 @@ void error2(char *source, int lineno, char *function, char *message);
 #define LOGPOS() \
 	cout("%s (%d) %s %.3f\n", __FILE__, __LINE__, __FUNCTION__, GetTickCount() / 1000.0)
 
+// sync > @ cout_h
 void cout(char *format, ...);
 char *xcout(char *format, ...);
+char *vxcout(char *format, va_list marker);
+void coutLongText(char *text);
+void coutLongText_x(char *text);
+// < sync
 
 int hasArgs(int count);
 int argIs(char *spell);
